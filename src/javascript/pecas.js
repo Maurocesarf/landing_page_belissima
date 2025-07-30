ef09fd0d52c2a017a575e7b4137ccc1bc956252c
Maurocesarf <maurocesarferreira4@gmail.com>
2025-07-29 22:33:41 -0300

    add pecas

---
+++ src/javascript/pecas.js
@@ -0,0 +1,29 @@
+ export const pecas = [
+                {
+                id: 1,
+                nome: "Colar coração vazado",
+                descricao: "teste descricao colar.",
+                preco: "R$55,00",
+                imagem: "src/images/colar_coracao_vazado.jpg",
+                imagem2: "src/images/colar_coracao_vazado_2.jpg",
+                esgotado: false
+                },
+                {
+                id: 2,
+                nome: "Colar flor",
+                descricao: "teste descricao colar flor.",
+                preco: "R$35,00",
+                imagem: "src/images/colar_flor.jpg",
+                imagem2: "",
+                esgotado: false
+                },
+                {
+                id: 3,
+                nome: "Duo brinco borboleta",
+                descricao: "teste descricao duo brinco borboleta.",
+                preco: "R$18,00",
+                imagem: "src/images/duo_brinco_borboleta.jpg",
+                imagem2: "src/images/duo_brinco_borboleta_2.jpg",
+                esgotado: false
+                },
+            ];

03dade3af6501db62281e1ba29dacc757ce248bc
Maurocesarf <maurocesarferreira4@gmail.com>
2025-07-31 22:05:10 -0300

    arrays products

--- src/javascript/pecas.js
+++ src/javascript/pecas.js
@@ -1,29 +1,188 @@
- export const pecas = [
+           const brincos = [
                 {
                 id: 1,
+                nome: "Brinco 3 corações",
+                descricao: "Brinco 3 Corações | Banhado a prata 50 milésimos, hipoalergênico, com dupla camada de verniz – brilho e proteção garantidos.",
+                preco: "R$37,90",
+                preco_av: "R$36,00",
+                imagem: "src/images/brinco_tres_coracoes.jpg",
+                imagem2: "src/images/brinco_tres_coracoes_2.jpg",
+                esgotado: false
+                },
+                {
+                id: 2,
+                nome: "Brinco coração torcido",
+                descricao: "Brinco coração torcido | Banhado a prata 50 milésimos, hipoalergênico, com dupla camada de verniz – brilho e proteção garantidos.",
+                preco: "R$37,90",
+                preco_av: "R$36,00",
+                imagem: "src/images/brinco_coracao_torcido.jpg",
+                imagem2: "src/images/brinco_coracao_torcido_2.jpg",
+                esgotado: false
+                },
+                {
+                id: 3,
+                nome: "Duo de brincos | coração cravejado + argola cravejada",
+                descricao: "Duo de brincos | Banhado a prata 50 milésimos, hipoalergênico, com dupla camada de verniz – brilho e proteção garantidos.",
+                preco: "R$50,90",
+                preco_av: "R$48,00",
+                imagem: "src/images/duo_brinco_coracao_cravejado.jpg",
+                imagem2: "src/images/duo_brinco_coracao_cravejado_2.jpg",
+                esgotado: false
+                },
+                {
+                id: 4,
+                nome: "Duo de brincos | borboleta + argola cravejada",
+                descricao: "Duo de brincos | Banhado a prata 50 milésimos, hipoalergênico, com dupla camada de verniz – brilho e proteção garantidos.",
+                preco: "R$50,90",
+                preco_av: "R$48,00",
+                imagem: "src/images/duo_brinco_borboleta.jpg",
+                imagem2: "src/images/duo_brinco_borboleta_2.jpg",
+                esgotado: false
+                },
+                {
+                id: 5,
+                nome: "Duo de brincos | estrela cravejada + argola",
+                descricao: "Duo de brincos | Banhado a prata 50 milésimos, hipoalergênico, com dupla camada de verniz – brilho e proteção garantidos.",
+                preco: "R$50,90",
+                preco_av: "R$48,00",
+                imagem: "src/images/duo_brinco_estrela_argola.jpg",
+                imagem2: "",
+                esgotado: false
+                },
+                {
+                id: 6,
+                nome: "Brinco trevo verde",
+                descricao: "Brinco trevo verde | Banhado a prata 50 milésimos, hipoalergênico, com dupla camada de verniz – brilho e proteção garantidos.",
+                preco: "R$37,90",
+                preco_av: "R$36,00",
+                imagem: "src/images/colar_trevo_cristal.jpg",
+                imagem2: "src/images/colar_trevo_cristal_2.jpg",
+                esgotado: false
+                }
+            ];
+
+            const pulseiras = [
+                {
+                id: 7,
+                nome: "Pulseira duplo fio gota cristal",
+                descricao: "Pulseira duplo fio gota cristal | Banhado a prata 50 milésimos, hipoalergênico, com dupla camada de verniz – brilho e proteção garantidos.",
+                preco: "R$41,90",
+                preco_av: "R$39,90",
+                imagem: "src/images/pulseira_duplo_fio.jpg",
+                imagem2: "",
+                esgotado: false
+                },
+                {
+                id: 8,
+                nome: "Pulseira dupla corações",
+                descricao: "Pulseira dupla corações | Banhado a prata 50 milésimos, hipoalergênico, com dupla camada de verniz – brilho e proteção garantidos.",
+                preco: "R$41,90",
+                preco_av: "R$39,90",
+                imagem: "src/images/pulseira_dupla_coracoes.jpg",
+                imagem2: "src/images/pulseira_dupla_coracoes_2.jpg",
+                esgotado: false
+                },
+                {
+                id: 9,
+                nome: "Pulseira infinito cravejada",
+                descricao: "Pulseira infinito cravejada | Banhado a prata 50 milésimos, hipoalergênico, com dupla camada de verniz – brilho e proteção garantidos.",
+                preco: "R$47,90",
+                preco_av: "R$45,60",
+                imagem: "src/images/pulseira_infinito.jpg",
+                imagem2: "",
+                esgotado: false
+                },
+                {
+                id: 10,
+                nome: "Pulseira de bolinhas",
+                descricao: "Pulseira de bolinhas | Banhado a prata 50 milésimos, hipoalergênico, com dupla camada de verniz – brilho e proteção garantidos.",
+                preco: "R$41,90",
+                preco_av: "R$39,90",
+                imagem: "src/images/pulseira_bolinhas.jpg",
+                imagem2: "src/images/pulseira_bolinhas_2.jpg",
+                esgotado: false
+                },
+]
+
+            const colares = [
+                {
+                id: 11,
+                nome: "Colar pedra rosa",
+                descricao: "Colar pedra rosa | Banhado a prata 50 milésimos, hipoalergênico, com dupla camada de verniz – brilho e proteção garantidos.",
+                preco: "R$47,90",
+                preco_av: "R$45,60",
+                imagem: "src/images/colar_pedra_rosa.jpg",
+                imagem2: "src/images/colar_pedra_rosa_2.jpg",
+                esgotado: false
+                },
+                {
+                id: 12,
+                nome: "Colar lua cravejada",
+                descricao: "Colar lua cravejada | Banhado a prata 50 milésimos, hipoalergênico, com dupla camada de verniz – brilho e proteção garantidos.",
+                preco: "R$50,90",
+                preco_av: "R$48,50",
+                imagem: "src/images/colar_lua_cravejada.jpg",
+                imagem2: "src/images/colar_lua_cravejada_2.jpg",
+                esgotado: false
+                },
+                {
+                id: 13,
                 nome: "Colar coração vazado",
-                descricao: "teste descricao colar.",
-                preco: "R$55,00",
+                descricao: "Colar coração vazado | Banhado a prata 50 milésimos, hipoalergênico, com dupla camada de verniz – brilho e proteção garantidos.",
+                preco: "R$47,90",
+                preco_av: "R$45,60",
                 imagem: "src/images/colar_coracao_vazado.jpg",
                 imagem2: "src/images/colar_coracao_vazado_2.jpg",
                 esgotado: false
                 },
                 {
-                id: 2,
-                nome: "Colar flor",
-                descricao: "teste descricao colar flor.",
-                preco: "R$35,00",
-                imagem: "src/images/colar_flor.jpg",
+                id: 14,
+                nome: "Colar trevo cristal",
+                descricao: "Colar trevo cristal | Banhado a prata 50 milésimos, hipoalergênico, com dupla camada de verniz – brilho e proteção garantidos.",
+                preco: "R$47,90",
+                preco_av: "R$45,60",
+                imagem: "src/images/colar_trevo_cristal.jpg",
+                imagem2: "src/images/colar_trevo_cristal_2.jpg",
+                esgotado: false
+                },
+                {
+                id: 15,
+                nome: "Colar trevo verde",
+                descricao: "Colar trevo verde | Banhado a prata 50 milésimos, hipoalergênico, com dupla camada de verniz – brilho e proteção garantidos.",
+                preco: "R$50,90",
+                preco_av: "R$48,50",
+                imagem: "src/images/colar_trevo_cristal.jpg",
+                imagem2: "src/images/colar_trevo_cristal_2.jpg",
+                esgotado: false
+                },
+                {
+                id: 16,
+                nome: "Colar infinito cravejado",
+                descricao: "Colar infinito cravejado | Banhado a prata 50 milésimos, hipoalergênico, com dupla camada de verniz – brilho e proteção garantidos.",
+                preco: "R$47,90",
+                preco_av: "R$45,60",
+                imagem: "src/images/colar_infinito.jpg",
                 imagem2: "",
                 esgotado: false
                 },
                 {
-                id: 3,
-                nome: "Duo brinco borboleta",
-                descricao: "teste descricao duo brinco borboleta.",
-                preco: "R$18,00",
-                imagem: "src/images/duo_brinco_borboleta.jpg",
-                imagem2: "src/images/duo_brinco_borboleta_2.jpg",
+                id: 17,
+                nome: "Colar coração lilás",
+                descricao: "Colar coração lilás | Banhado a prata 50 milésimos, hipoalergênico, com dupla camada de verniz – brilho e proteção garantidos.",
+                preco: "R$47,90",
+                preco_av: "R$45,60",
+                imagem: "src/images/colar_coracao_lilas.jpg",
+                imagem2: "",
                 esgotado: false
                 },
-            ];
+                {
+                id: 18,
+                nome: "Colar flor",
+                descricao: "Colar flor | Banhado a prata 50 milésimos, hipoalergênico, com dupla camada de verniz – brilho e proteção garantidos.",
+                preco: "R$47,90",
+                preco_av: "R$45,60",
+                imagem: "src/images/colar_flor.jpg",
+                imagem2: "",
+                esgotado: false
+                },
+]

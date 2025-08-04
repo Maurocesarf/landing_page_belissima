a90017f1059b99116f4bbb9eb5d44ce5c81c1503
Maurocesarf <maurocesarferreira4@gmail.com>
2025-08-04 20:59:02 -0300

    add sections

--- src/javascript/script.js
+++ src/javascript/script.js
@@ -40,7 +40,7 @@ $(document).ready(function() {
         distance: '20%'
     });
 
-    ScrollReveal().reveal('.dish', {
+    ScrollReveal().reveal('.card', {
         origin: 'left',
         duration: 2000,
         distance: '20%'
